deba58dca41402c37128081a7dc46f662e640b9d
Jonny Anderson <Jonny@Rolf.local>
2018-08-01 10:46:00 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require( 'express' );
+const hbs = require( 'hbs' );
+
+const fs = require( 'fs' );
+
+var app = express();
+
+hbs.registerPartials( __dirname + '/views/partials' );
+
+app.set( 'view engine', 'hbs' );
+
+app.use( express.static( __dirname + '/public' ) );
+
+app.use( ( req, res, next ) => {
+
+	var log = {
+		time : new Date().toString(),
+		method : req.method,
+		url : req.url
+	};
+
+	fs.appendFile( 'server.log', JSON.stringify( log ) + '\n', ( err ) => {
+
+	});
+
+	next();
+});
+
+hbs.registerHelper( 'getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper( 'screamIt', ( message ) => {
+	return message.toUpperCase();
+});
+
+app.get( '/', ( req, res ) => {
+	res.render( 'home.hbs', {
+		pageTitle : 'Home Page',
+		message : 'Welcome Message'
+	});
+});
+
+app.get( '/about', ( req, res ) => {
+	res.render( 'about.hbs', {
+		pageTitle : 'About Page'
+	});
+});
+
+app.get( '/bad', ( req, res ) => {
+	//console.log( req );
+	
+	res.send({
+		errorMessage : 'Request Failed'
+	});
+});
+
+app.listen( 3000 );
